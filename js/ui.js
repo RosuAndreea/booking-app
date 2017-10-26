f7f51dd3cb6e111a1e0d82d6a2c27d0dabe90565
Andreea Alupoaei <andreea.alupoaei@assist.ro>
2017-10-26 10:13:44 +0300

    register step

---
+++ js/ui.js
@@ -0,0 +1,6 @@
+$('.calendar').datepicker({
+   onSelect: function(dateText, inst) { 
+      var date = dateText; // the selected date from calendar
+      window.mySelection.selected_date = date;
+      window.renderSelectedDate();
+   }});
