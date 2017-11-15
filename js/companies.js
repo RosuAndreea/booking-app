23e08ec1eb4fdbe5b136298c924f690a2c5f4977
Andreea Alupoaei <andreea.alupoaei@assist.ro>
2017-11-15 14:12:09 +0200

    take companies from db and populate the Companies page

--- js/companies.js
+++ js/companies.js
@@ -1,3 +1,36 @@
 (function () {
-    
+    var $card = $('.row');
+
+    function addTemplate(item) {
+        $card.append('<div class="col-md-3"><div class="card">'
+            +'<div class="view overlay hm-white-slight">'
+            +'<img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%28131%29.jpg" class="img-fluid" alt="">'
+            +'<a><div class="mask"></div></a></div>'
+            +'<div class="card-body">'
+            +'<h4 class="card-title">' + item.name + '</h4>'
+            +'<p class="card-text">' + item.description + '...</p>'
+            +'<a href="#" class="black-text d-flex flex-row-reverse hvr-pulse">'
+            +'<button class="waves-effect p-2">Book Now <i class="fa fa-chevron-right"></i></button>'
+            +'</a></div></div></div>'
+        );
+    }
+
+    $.ajax({
+        type: "GET",
+        url: ApiUrl + 'companies',
+        // headers: {
+        //     "Access-Control-Allow-Origin": "*"
+        // },
+        success: function (companies){
+            console.log('companies ', companies)
+            $.each(companies, function (i, item){
+                addTemplate(item);
+            });
+        },
+        error: function (err){
+            console.log('Err ', err);
+            //alert('Error loading services');
+        }
+
+    });
 })();
