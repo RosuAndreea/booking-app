aaf15d78fb049ad4b876e67a4487438dc7c9d9dc
Andreea Alupoaei <andreea.alupoaei@assist.ro>
2017-11-20 09:53:17 +0200

    add buttons and actions for buttons in calendar section

--- js/main.js
+++ js/main.js
@@ -1,20 +1,16 @@
 (function (){
     window.ApiUrl = "http://localhost:3000/";
-
-    $('#select-comp').delegate('#selectOff','click', function() {
-        $('#select-comp').addClass('hidden');
-        $('#select-off').removeClass('hidden');
-    });
-
-
-    $('#select-off').delegate('#selectServ','click', function() {  
-        $('#select-off').hide();
-        $('#select-serv').removeClass('hidden');
-    });
-
+    
+    // Back to companies list
     $('#select-off').delegate('#selectCompany','click', function() {  
         $('#select-off').hide();
         $('#select-comp').removeClass('hidden');
+    });
+
+    // Select an office
+    $('#select-comp').delegate('#selectOff','click', function() {
+        $('#select-comp').addClass('hidden');
+        $('#select-off').removeClass('hidden');
     });
 
     $('#select-serv').delegate('#backSelectOff','click', function() {
@@ -22,8 +18,26 @@
         $('#select-off').show();
     });
 
+    // Select services
+    $('#select-off').delegate('#selectServ','click', function() {  
+        $('#select-off').hide();
+        $('#select-serv').removeClass('hidden');
+    });
+
+    $('#select-date').delegate('#backToService','click',function() {
+        $('#select-date').addClass('hidden');
+        $('#select-serv').removeClass('hidden');
+    })
+
+    // Select a date
     $('#select-serv').delegate('.selectDate','click', function() {
         $('#select-serv').addClass('hidden');
         $('#select-date').removeClass('hidden');
+    });
+
+    //Complete the register form
+    $('#select-date').delegate('#registerForm','click',function() {
+        $('#select-date').addClass('hidden');
+        $('#register-form').removeClass('hidden');
     });
 })();
